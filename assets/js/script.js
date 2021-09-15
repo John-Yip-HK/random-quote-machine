59b558074794b3a4f9b43a1fc590675f17c5990b
John-Yip-HK <yip4work@gmail.com>
2021-09-15 12:56:39 +0800

    Project can be submitted.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,39 @@ let quote = "";
 let author = "";
 const textElement = document.getElementById("text");
 const authorElement = document.getElementById("author");
+const transitionDuration = 1; // in seconds
 
-const parseSentence = (sentence, placeHolder) =>
+let currColor = null;
+const baseColor = "#495057";
+let prevColorId = -1;
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+
+const getWindowWidth = () =>
+  Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+
+const parseSentenceWhiteSpace = (sentence, placeHolder) =>
   sentence.replace(/\s/g, placeHolder);
 
 const getQuote = () => {
   setTimeout(
     () => {
-      fetch("https://api.quotable.io/random?maxLength=50")
+      let url = "https://api.quotable.io/random";
+      if (getWindowWidth() < 992) url += "?maxLength=50";
+
+      fetch(url)
         .then((response) => response.json())
         .then((data) => {
           quote = data.content;
@@ -20,14 +45,35 @@ const getQuote = () => {
 
           authorElement.innerHTML = author;
           authorElement.style.opacity = 1;
+
+          let colorId = -1;
+
+          do {
+            colorId = Math.floor(Math.random() * colors.length);
+          } while (prevColorId === colorId);
+
+          prevColorId = colorId;
+          currColor = colors[colorId];
+
+          document.body.style.backgroundColor = currColor;
+          document.body.style.color = currColor;
+          document.getElementById("quote-box").style.color = currColor;
+          document.getElementById("new-quote").style.backgroundColor =
+            currColor;
+          document.getElementById("new-quote").style.borderColor = currColor;
+          for (let icon of document.getElementsByClassName("fab")) {
+            icon.style.color = currColor;
+          }
         })
         .catch((err) => {
-          console.log(err);
+          alert(
+            `${err.message}. Please check your network connection and try again.`
+          );
         });
     },
     textElement.style.opacity === 0 || authorElement.style.opacity === 0
       ? 0
-      : 1000
+      : transitionDuration * 1000
   );
 
   if (textElement.style.opacity > 0 || authorElement.style.opacity > 0) {
@@ -36,16 +82,46 @@ const getQuote = () => {
   }
 };
 
-window.onload = getQuote;
+const changeIconMargin = () => {
+  if (getWindowWidth() >= 568) {
+    for (let icon of document.getElementsByClassName("fab")) {
+      icon.style.display = "inline block";
+      icon.style.margin = "0 6px 0 0";
+    }
+  } else {
+    for (let icon of document.getElementsByClassName("fab")) {
+      icon.style.display = "inline";
+      icon.style.margin = "0";
+    }
+  }
+};
+
+// Init function.
+window.onload = () => {
+  document.body.style.transition = "background-color 1s, color 1s";
+  document.getElementById("quote-box").style.transition = "color 1s";
+  document.getElementById("new-quote").style.transition =
+    "background-color 1s, border-color 1s, opacity 0.3s";
+  textElement.style.transition = "opacity 1s";
+  authorElement.style.transition = "opacity 1s";
+  for (let icon of document.getElementsByClassName("fab")) {
+    icon.style.transition = "opacity 0.3s, color 1s";
+  }
+
+  window.addEventListener("resize", changeIconMargin);
+
+  changeIconMargin();
+  getQuote();
+};
 
 const genPost = (element) => {
-  const parsedQuote = parseSentence(quote, "%20");
-  const parsedAuthor = parseSentence(author, "");
+  const parsedQuote = parseSentenceWhiteSpace(quote, "%20");
+  const parsedAuthor = parseSentenceWhiteSpace(author, "");
 
   if (element.id === "tweet-quote") {
     element.setAttribute(
       "href",
-      `https://twitter.com/intent/tweet?text=${parsedQuote}&hashtags=${parsedAuthor}`
+      element.href + `?text=${parsedQuote}&hashtags=${parsedAuthor}`
     );
   } else if (element.id === "tumblr-quote") {
     const date = new Date();
@@ -59,4 +135,6 @@ const genPost = (element) => {
   } else {
     alert("Unknown link is clicked. Please report to us.");
   }
+
+  element.style.color = currColor;
 };
